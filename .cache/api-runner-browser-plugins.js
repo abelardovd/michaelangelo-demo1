875dd011d7b4a4dc532eb0992fa151707e35da36
abelardo <ivo_v97@hotmail.com>
2018-10-11 20:24:31 -0500

    commit 2

---
+++ .cache/api-runner-browser-plugins.js
@@ -0,0 +1,4 @@
+module.exports = [{
+      plugin: require('/Users/yadiraeloisadavilacorona/Desktop/Trabajo/michaelangelo-demo1/node_modules/gatsby-plugin-offline/gatsby-browser'),
+      options: {"plugins":[]},
+    }]
---
+++ .cache/async-requires.js
@@ -0,0 +1,11 @@
+// prefer default export if available
+const preferDefault = m => m && m.default || m
+
+exports.components = {
+  "component---cache-dev-404-page-js": () => import("/Users/yadiraeloisadavilacorona/Desktop/Trabajo/michaelangelo-demo1/.cache/dev-404-page.js" /* webpackChunkName: "component---cache-dev-404-page-js" */),
+  "component---src-pages-404-js": () => import("/Users/yadiraeloisadavilacorona/Desktop/Trabajo/michaelangelo-demo1/src/pages/404.js" /* webpackChunkName: "component---src-pages-404-js" */),
+  "component---src-pages-index-js": () => import("/Users/yadiraeloisadavilacorona/Desktop/Trabajo/michaelangelo-demo1/src/pages/index.js" /* webpackChunkName: "component---src-pages-index-js" */)
+}
+
+exports.data = () => import("/Users/yadiraeloisadavilacorona/Desktop/Trabajo/michaelangelo-demo1/.cache/data.json")
+
